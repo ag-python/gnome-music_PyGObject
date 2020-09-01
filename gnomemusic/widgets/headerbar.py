GNOME/gnome-music

CI: Add static type checking pass
Also:
* Ignore initial propertyerrors
* Add relevant .gitignore
* Add some required types

gnomemusic/widgets/headerbar.py
@@ -55,7 +55,7 @@ class SelectionBarMenuButton(Gtk.MenuButton):
         """
         return self._selected_items_count
 
-    @selected_items_count.setter
+    @selected_items_count.setter  # type: ignore
     def selected_items_count(self, value):
         """Set the number of items selected
 
@@ -153,7 +153,7 @@ class HeaderBar(Gtk.HeaderBar):
         """
         return self._selection_mode
 
-    @selection_mode.setter
+    @selection_mode.setter  # type: ignore
     def selection_mode(self, mode):
         """Set the selection mode
 
@@ -178,7 +178,7 @@ class HeaderBar(Gtk.HeaderBar):
         """
         return self._state
 
-    @state.setter
+    @state.setter  # type: ignore
     def state(self, value):
         """Set state of the of widget
 

ag-python/gnome-music_PyGObject

gnomemusic/widgets/headerbar.py
@@ -55,7 +55,7 @@ class SelectionBarMenuButton(Gtk.MenuButton):
         """
         return self._selected_songs_count
 
-    @selected_songs_count.setter
+    @selected_songs_count.setter  # type: ignore
     def selected_songs_count(self, value):
         """Set the number of songs selected
 
@@ -157,7 +157,7 @@ class HeaderBar(Gtk.HeaderBar):
         """
         return self._selection_mode
 
-    @selection_mode.setter
+    @selection_mode.setter  # type: ignore
     def selection_mode(self, mode):
         """Set the selection mode
 
@@ -182,7 +182,7 @@ class HeaderBar(Gtk.HeaderBar):
         """
         return self._state
 
-    @state.setter
+    @state.setter  # type: ignore
     def state(self, value):
         """Set state of the of widget
 